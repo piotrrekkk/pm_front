62d9ad8a726270277768e2e864224d69833ea023
Piotr Rosa <piotrek0rosa@gmail.com>
2020-01-22 19:56:04 +0100

    wip

--- src/components/graphs.js
+++ src/components/graphs.js
@@ -3,10 +3,12 @@ import _ from 'underscore';
 
 export class Graphs extends React.Component {
     render() {
-        <div>
-            PM 2.5: {_.last(this.props.pm25)}
-            <br/>
-            PM 10: {_.last(this.props.pm10)}
-        </div>
+        return (
+            <div>
+                PM 2.5: {_.last(this.props.pm25)}
+                <br />
+                PM 10: {_.last(this.props.pm10)}
+            </div>
+        )
     }
 }
